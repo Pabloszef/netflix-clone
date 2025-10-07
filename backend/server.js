9c6b3834ee70df4f10cde29b33242aa418581af9
Pablo <pawel.zaremba013@gmail.com>
2025-10-07 19:42:44 +0200

    Finish fetching movies from TMDB

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
 import express from "express";
 import {ENV_VARS} from "./config/envVars.js";
-
+import {connectDB} from "./config/db.js";
 
 import authRoutes from "./routes/auth.route.js";
-import {connectDB} from "./config/db.js";
+import movieRoutes from "./routes/movie.route.js";
 
 
 const PORT = ENV_VARS.PORT;
-
 
 const app = express();
 
 app.use(express.json())
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/movie", movieRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on the port: " + PORT)
     connectDB()
 })
+
+
