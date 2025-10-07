4904334d7f490ebfae4cf031474eceeae6929516
Pablo <pawel.zaremba013@gmail.com>
2025-10-07 21:09:50 +0200

    Added Protect Route Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {ENV_VARS} from "./config/envVars.js";
 import {connectDB} from "./config/db.js";
+import {protectRoute} from "./middleware/protectRoute.js";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -12,10 +14,11 @@ const PORT = ENV_VARS.PORT;
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/movie", movieRoutes)
-app.use("/api/v1/tv", tvRoutes)
+app.use("/api/v1/movie", protectRoute, movieRoutes)
+app.use("/api/v1/tv", protectRoute, tvRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on the port: " + PORT)
