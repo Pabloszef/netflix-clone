d0ffbdd25dfe2fefba2b1ffe7320b37e758dbf28
Pablo <pawel.zaremba013@gmail.com>
2025-10-09 18:58:00 +0200

    Finish Search Routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 
 const PORT = ENV_VARS.PORT;
@@ -19,6 +20,7 @@ app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute, tvRoutes)
+app.use("/api/v1/search", protectRoute, searchRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on the port: " + PORT)
